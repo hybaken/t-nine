bc45234b31f127a44cbf7f6bcaa90141159f76ca
Patrik Kempec <patrik.kempec@gmail.com>
2020-08-03 23:35:36 +0200

    Removing node_modules

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const react = express.static(path.join(__dirname, '../client/build'))
 
 var app = express();
 
@@ -21,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(react)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
